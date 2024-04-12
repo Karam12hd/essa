220315c4388ab2a3b904c3da5b19232f78e9f94b
Karam12hd <142044749+Karam12hd@users.noreply.github.com>
2024-04-12 22:32:40 +0300

    Add files via upload

---
+++ phpfunc/config.php
@@ -0,0 +1,6 @@
+<?php
+$client_id = "";
+$secret_id = "";
+$scopes = "identify guilds.join";
+$redirect_url = "https://essafuscator.net/login.php";
+$bot_token = "MTIyNzYzODA2NTA2NjAyMDg4NQ.GsQwLI.GA0FZtXAGWpTl52tJVRQU6ZXJWHpopWGpKb-9U";
